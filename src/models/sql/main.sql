00952a41d53db881f8712505eed5662a4c87a469
interesnij <ochkarik1983@mail.ru>
2025-02-19 15:01:00 +0300

    J

--- src/models/sql/main.sql
+++ src/models/sql/main.sql
@@ -139,5 +139,5 @@ CREATE TABLE email_verification_token (
     id         BYTEA PRIMARY KEY,
     email      TEXT UNIQUE NOT NULL,
     expires_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp
+    created_at TIMESTAMP NOT NULL
 ); 
